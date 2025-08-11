3d459a66b22cc63d6887027201d862f988c5ccbf
Muhammad Fahad Zaman <fahadzaman664@gmail.com>
2025-08-12 00:13:03 +0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+import { BrowserRouter,Navigate, Routes , Route} from 'react-router-dom'
+import Auth from './pages/auth'
+import Chat from './pages/chat'
+import Profile from './pages/profile'
+const App = () => {
+  return (
+
+<>
+<BrowserRouter>
+<Routes>
+
+<Route path="/auth" element={<Auth />} />
+<Route path="*" element={<Navigate to="/auth"/>} />
+  <Route  path="/chat" element={<Chat />} />
+    <Route  path="/profile" element={<Profile />} />
+
+
+</Routes>
+</BrowserRouter>
+</>
+  )
+}
+
+export default App
